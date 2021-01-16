befbd3112c90c5f331ca24a1b11a299ed08643bb
Somesh <samchopz5@gmail.com>
2021-01-17 02:44:39 +0530

    added-node

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const mongoose= require('mongoose');
 
 const app= express();
 
-mongoose.connect("mongodb+srv://somesh:"+process.env.MONGO_ATLAS_PW+"@cluster0.fn5lq.mongodb.net/node-angular?retryWrites=true&w=majority").then(()=>{
+mongoose.connect("mongodb+srv://somesh:YFprTZKuhXbBBhxC@cluster0.fn5lq.mongodb.net/node-angular?retryWrites=true&w=majority").then(()=>{
   console.log("Connected Successfully");
 }).catch(()=>{
   console.log("Connection Failed");
